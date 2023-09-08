fb36bf97608b5bf96c13eb2fdb3257bde1e86f69
shreya077bx <144240282+shreya077bx@users.noreply.github.com>
2023-09-08 10:49:23 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
     <h1 className="mainTitle">Money transfer app</h1>
     <h2 className="description">Send fake money to fake people.</h2>
     <h1 className="balance">
-      Balance: <span className="balance__number">€ {props.balance} </span>
+      Balance: <span className="balance__number">₹ {props.balance} </span>
     </h1>
   </Fragment>
 );
